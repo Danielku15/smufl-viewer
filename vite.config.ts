4c6c1280b91934523ef8729e0f8c0742aee28018
Danielku15 <danielku15@coderline.net>
2025-08-11 14:41:38 +0200

    build: Configure base url

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,7 @@ import tsconfigPaths from 'vite-tsconfig-paths';
 
 export default defineConfig(() => {
     return {
+        base: process.env.BASE_URL,
         plugins: [
             tsconfigPaths()
         ]
