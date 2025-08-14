e93e894421a2c96c767f0688d33aa7f1f8cbe251
Danielku15 <danielku15@coderline.net>
2025-08-15 01:21:26 +0200

    fix: corrected base URL

--- vite.config.ts
+++ vite.config.ts
@@ -1,19 +1,19 @@
 import { defineConfig } from 'vite';
 import tsconfigPaths from 'vite-tsconfig-paths';
 
-function trimSlash(v: string | undefined) {
+function ensureSlash(v: string | undefined) {
     if (!v) {
-        return v;
+        return '/';
     }
     if (v.endsWith('/')) {
-        return v.substring(0, v.length - 1);
+        return v;
     }
-    return v;
+    return `${v}/`;
 }
 
 export default defineConfig(() => {
     return {
-        base: trimSlash(process.env.BASE_URL),
+        base: ensureSlash(process.env.BASE_URL),
         plugins: [
             tsconfigPaths()
         ]
